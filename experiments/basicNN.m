873d33496e8596030d3149651ea689b2608a5cae
Clive Chan <cc@clive.io>
2015-08-24 16:03:03 -0400

    Continued work with Matlab/Octave (learning, basically)

--- experiments/basicNN.m
+++ experiments/basicNN.m
@@ -1,13 +1,15 @@
 
-inputSize = 10;
-layerSizes = [15;15;5];
+inputSize = 15;
+layerSizes = [10,5,15];
 
 
 nndata = {rand(layerSizes(1), inputSize)};
-for i = 2:size(layerSizes)(1)
+for i = 2:length(layerSizes)
 	nndata = [nndata {rand(layerSizes(i),layerSizes(i-1))}];
 end
 
-for i = 1:size(nndata)
-	nndata{i}
+vector = [1:15]';
+newvec = nndata{1} * vector;
+for i = 2:length(nndata)
+	newvec = nndata{i} * (1./(1+e.^-newvec));
 end
